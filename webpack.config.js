1846f67abc55005273fa54c294164bdb38dc926a
Jean Claveau <jean@Jean-Thinkpad>
2019-04-03 18:39:32 +0200

    Webpack working

--- webpack.config.js
+++ webpack.config.js
@@ -4,40 +4,43 @@ const pkg = require('./package.json');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+    entry: './src/index.js',
+    mode: 'production', // 'development' https://webpack.js.org/configuration/mode/
 
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'jquery.nouislider-range-input.js',
-    libraryTarget: 'umd',
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-    ],
-  },
-
-  plugins: [
-    new webpack.BannerPlugin({
-      banner: `${pkg.name}\n${pkg.version}\nTested with jQuery 1.12+\n${pkg.repository.url}\nLicense: ${pkg.license}`,
-    }),
-    new UglifyJsPlugin({
-      beautify: true,
-      mangle: false,
-    }),
-  ],
-
-  externals: {
-    jquery: {
-      commonjs: 'jquery',
-      commonjs2: 'jquery',
-      amd: 'jquery',
-      root: 'jQuery',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'jquery.nouislider-range-input.js',
+        libraryTarget: 'umd',
     },
-  },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+
+    plugins: [
+        // new webpack.BannerPlugin({
+        // banner: `${pkg.name}\n${pkg.version}\nTested with jQuery 1.12+\n${pkg.repository.url}\nLicense: ${pkg.license}`,
+        // }),
+        // new UglifyJsPlugin({
+        // beautify: true,
+        // mangle: false,
+        // }),
+    ],
+
+    externals: {
+        jquery: {
+            commonjs: 'jquery',
+            commonjs2: 'jquery',
+            amd: 'jquery',
+            root: 'jQuery',
+        },
+        nouislider: 'nouislider',
+        wnumb: 'wnumb',
+    },
 };
